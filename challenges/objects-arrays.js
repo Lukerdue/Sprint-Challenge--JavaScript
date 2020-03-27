008cc96b92d135e26b4adea57d838a23ee84df5e
Jamie <jamielynjenks@gmail.com>
2020-03-27 09:36:36 -0600

    Dino objects and console logs done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,7 +5,6 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -14,21 +13,42 @@
 
 // Using your dinosaur objects, log answers to these questions:
 
+const tyrannosaurus ={
+  diet: 'carnivorous',
+  weight: 7000,
+  length: 12,
+  period: 'late cretaceous',
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
+const stegosaurus = {
+  diet: 'herbivorous',
+  weight: 2000,
+  length: 9,
+  period: 'Late Jurassic'
+}
+const velociraptor = {
+  diet: 'carnivorous',
+  weight: 15,
+  length: 1.8,
+  period: 'Late Cretaceous'
+}
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight + 'kg');
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length + 'm');
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
